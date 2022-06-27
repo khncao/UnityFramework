40c9659f1252a8d63acd268bab816c0ffc38fc1d
Kelvin Cao <khncao@gmail.com>
2022-06-27 15:06:59 -0500

    Feat: SerializableDict draw containers & lists

--- com.minus4kelvin.core/Editor/SerializableDictionaryDrawer.cs
+++ com.minus4kelvin.core/Editor/SerializableDictionaryDrawer.cs
@@ -22,6 +22,7 @@ public class SerializableDictionaryDrawer : PropertyDrawer {
             var listProp = property.FindPropertyRelative("list");
             list = new ReorderableList(property.serializedObject, listProp, true, false, true, true);
             list.drawElementCallback = DrawListItems;
+            list.elementHeightCallback = ListItemHeight;
         }
 
         var firstLine = position;
@@ -65,42 +66,81 @@ public class SerializableDictionaryDrawer : PropertyDrawer {
         var keyProp   = element.FindPropertyRelative("Key");
         var valueProp = element.FindPropertyRelative("Value");
 
-        elementIndex.text = $"Element {index}";
+        elementIndex.text = $"{index}";
         /*var label =*/ EditorGUI.BeginProperty(rect, elementIndex, element);
 
-        var prevLabelWidth = EditorGUIUtility.labelWidth;
+        // auto expand draw if objects or lists
+        if(keyProp.isExpanded || valueProp.isExpanded) {
+            rect = DrawListProperty(rect, index, isActive, isFocused, keyProp);
 
-        EditorGUIUtility.labelWidth = 75;
+            // janky workaround for key container objects
+            if(keyProp.isExpanded) 
+                rect.y -= EditorGUIUtility.singleLineHeight;
+            rect.y += EditorGUIUtility.singleLineHeight;
+            
+            rect = DrawListProperty(rect, index, isActive, isFocused, valueProp);
+        }
+        else { // single-line, spaced key & value if possible
+            var prevLabelWidth = EditorGUIUtility.labelWidth;
+            EditorGUIUtility.labelWidth = 75;
 
-        var rect0 = rect; //EditorGUI.PrefixLabel(rect, GUIUtility.GetControlID(FocusType.Passive), label);
+            var rect0 = rect; 
 
-        var halfWidth = rect0.width / 2f;
-        rect0.width = halfWidth;
-        rect0.y += 1f;
-        rect0.height -= 2f;
+            var halfWidth = rect0.width / 2f;
+            rect0.width = halfWidth;
+            rect0.y += 1f;
+            rect0.height -= 2f;
 
+            EditorGUIUtility.labelWidth = 40;
 
-        EditorGUIUtility.labelWidth = 40;
+            EditorGUI.BeginChangeCheck();
+            EditorGUI.PropertyField(rect0, keyProp);
+            
+            rect0.x += halfWidth + 4f;
 
-        EditorGUI.BeginChangeCheck();
-        EditorGUI.PropertyField(rect0, keyProp);
-
-        rect0.x += halfWidth + 4f;
-
-        EditorGUI.PropertyField(rect0, valueProp);
-
-        EditorGUIUtility.labelWidth = prevLabelWidth;
+            EditorGUI.PropertyField(rect0, valueProp);
+            EditorGUIUtility.labelWidth = prevLabelWidth;
+            
+        }
 
         EditorGUI.EndProperty();
     }
+    GUIContent emptyLabel = new GUIContent("");
+
+    Rect DrawListProperty(Rect rect, int index, bool isActive, bool isFocused, SerializedProperty property) {
+        EditorGUI.PropertyField(rect, property, includeChildren:true);
+        if(property.isExpanded)
+            rect.y += EditorGUI.GetPropertyHeight(property, true);
+        return rect;
+    }
+
+    float ListItemHeight(int index) {
+        var height = EditorGUIUtility.singleLineHeight;
+        if(index >= list.serializedProperty.arraySize)
+            return height;
+
+        SerializedProperty elementProp = list.serializedProperty.GetArrayElementAtIndex(index);
+
+        var keyProp   = elementProp.FindPropertyRelative("Key");
+        if(keyProp.isExpanded) 
+            height += EditorGUI.GetPropertyHeight(keyProp, true);
+
+        var valueProp = elementProp.FindPropertyRelative("Value");
+        if(valueProp.isExpanded) 
+            height += EditorGUI.GetPropertyHeight(valueProp, true);
+
+        return height;
+    }
 
     public override float GetPropertyHeight(SerializedProperty property, GUIContent label) {
-        if (property.isExpanded) {
+        if (property.isExpanded && list != null) {
             var listProp = property.FindPropertyRelative("list");
-            if (listProp.arraySize < 2)
-                return EditorGUIUtility.singleLineHeight + 52f;
-            else
-                return EditorGUIUtility.singleLineHeight + 23f * listProp.arraySize + 29;
+
+            return EditorGUIUtility.singleLineHeight + list.GetHeight();
+            // if (listProp.arraySize < 2)
+            //     return EditorGUIUtility.singleLineHeight + 52f;
+            // else
+            //     return EditorGUIUtility.singleLineHeight + 23f * listProp.arraySize + 29;
         }
         else
             return EditorGUIUtility.singleLineHeight;
